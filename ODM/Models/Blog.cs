96c2065776a947ea6b84fced8ccc22b0752ae48a
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-06-21 01:25:29 +0400

    Add title and content to the blog model

--- ODM/Models/Blog.cs
+++ ODM/Models/Blog.cs
@@ -4,6 +4,8 @@ namespace ODM.Models
 	public class Blog
 	{
 		public int id { get; set; }
+        public string title {get; set;} = "Title";
+        public string content {get; set;} = "Some content";
 	}
 }
 
