c56c6291c85eba9e15d1af37411ec0e9a5ad216f
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-06-28 22:34:22 +0400

    Create ContextManager.cs

---
+++ ODM/Context/ContextManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ODM.Context
+{
+    public class ContextManager : DbContext
+    {
+        private const string DATABASEFILE = "database.db";
+
+        public string DbPath {get;}
+        public ContextManager()
+        {
+            var path = @"full/path/to/your/project";
+            DbPath = Path.Join(path, DATABASEFILE);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+	}
+}
+
