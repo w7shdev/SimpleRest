a9e391030485ab2b62f6807284818db59cc76e9b
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-06-28 22:34:39 +0400

    Extend ContextManager

--- ODM/Context/BlogContext.cs
+++ ODM/Context/BlogContext.cs
@@ -4,21 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using ODM.Models;
 namespace ODM.Context
 {
-	public class BlogContext : DbContext
+	public class BlogContext : ContextManager
 	{
-        private const string DATABASEFILE = "database.db";
 
-        public string DbPath {get;}
-		public BlogContext()
+		public BlogContext():base()
 		{
-
-            var path = @"full/path/to/your/project";
-            DbPath = Path.Join(path, DATABASEFILE);
 		}
         public DbSet<Blog>? Blogs { get; set;}
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
 	}
 }
 
